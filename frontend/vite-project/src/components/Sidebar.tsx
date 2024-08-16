abb15766f12a2add11d7ab1dfc47043dfa2e3e3c
Etinosa Izekor <izekoretinosa15@gmail.com>
2024-08-16 17:15:29 +0300

    Add sidebar items

--- frontend/vite-project/src/components/Sidebar.tsx
+++ frontend/vite-project/src/components/Sidebar.tsx
@@ -1,9 +1,21 @@
-export default function Sidebar() {
-    return (
-        <aside className="h-screen bg-sky-950 w-28">
-            <div>
+import { Link } from "react-router-dom";
 
-            </div>
-        </aside>
-    )
+export default function Sidebar() {
+  return (
+    <aside className="h-screen bg-sky-950 w-40 mt">
+      <div className="mt-40">
+        <ul className="flex flex-col gap-5">
+          <li>
+            <Link to="#">Classes</Link>
+          </li>
+          <li>
+            <Link to="#">Courses</Link>
+          </li>
+          <li>
+            <Link to="#">Students</Link>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
 }

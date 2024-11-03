b3a08a99f08ebfb20ab2312a6dd4010886a76e74
Etinosa Izekor <izekoretinosa15@gmail.com>
2024-11-03 07:43:01 +0200

    feat: Sign out of the application on token expiration

--- frontend/vite-project/src/api/baseApi.ts
+++ frontend/vite-project/src/api/baseApi.ts
@@ -1,11 +1,38 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { redirect } from "react-router-dom";
+import { displayNotification } from "../components/notifications";
+
+const baseQueryWithAuth = async (
+  args: any,
+  api: BaseQueryApi,
+  extraOptions = {}
+) => {
+  // Use fetchBaseQuery to make the actual request
+  const result = await fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_SERVER_URL,
+    credentials: "include",
+  })(args, api, extraOptions);
+
+  // Check for 401 Unauthorized status
+  if (result.error && result.error.status === 401) {
+    displayNotification({
+      message: "Your session has expired",
+      title: "Session expired",
+      type: "error",
+    });
+    redirect("/login");
+  }
+
+  return result;
+};
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_SERVER_URL,
-    credentials: "include",
-  }),
-  endpoints: (builder) => ({}),
+  baseQuery: baseQueryWithAuth,
+  endpoints: () => ({}),
   tagTypes: ["Student", "Course", "Class"],
 });
